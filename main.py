c960bc19dead096e0aa8eedf22dbb42a10f08f60
raza.mehar <raza.mehar@gmail.com>
2023-04-19 05:26:26 +0500

    Added main file

---
+++ main.py
@@ -0,0 +1,10 @@
+#from add import add
+from subt import subtract
+from multiply import multiplication
+
+if __name__ == '__main__':
+    a = 6
+    b = 4
+    #print(add(a, b))
+    print(subtract(a, b))
+    print(multiplication(a, b))
